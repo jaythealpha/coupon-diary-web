e585daa0937165409dcfcf0ed833790333562f25
제이 <jay@jeiui-MacBookAir.local>
2026-08-16 16:12:25 +0900

    체험판 갱신 2026-08-16 16:12

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2134395376" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1927207495" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
